bc9da59df7308c9479a3f9bd72337a5e0a9c0ac5
Ayush <ayush@Ayushs-MacBook-Air-2.local>
2022-01-18 21:07:23 +0530

    bug fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,21 @@
 module.exports = {
-    siteMetadata: {
-      title: `ayush-portfolio2.0`,
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    title: `ayush-portfolio2.0`,
+    // siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Ayush`,
+        start_url: `/`,
+        background_color: `#0c0c0c`,
+        theme_color: `#0c0c0c`,
+        display: `standalone`,
+        icon: `src/images/logo/ayush.png`,
+        crossOrigin: `use-credentials`, // `use-credentials` or `anonymous`
+      },
     },
-    plugins: [
-
-    ]
-}
+  ],
+};
